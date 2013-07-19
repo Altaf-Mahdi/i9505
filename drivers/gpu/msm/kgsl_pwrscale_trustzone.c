f15817754b3709b3faa1b2abc5a81b14add3c2b8
Altaf-Mahdi <altaf.mahdi@gmail.com>
2013-07-19 02:14:30 +0100

    Revert "KGSL: simple governor: expose tunables to sysfs interface via module_param"
    This reverts commit c3209c9cb647b6c113d2b88f7ac7709eaa7cbc25.

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -23,10 +23,6 @@
 #include "kgsl_pwrscale.h"
 #include "kgsl_device.h"
 
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-#include <linux/module.h>
-#endif
-
 #define TZ_GOVERNOR_PERFORMANCE 0
 #define TZ_GOVERNOR_ONDEMAND    1
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
@@ -151,11 +147,8 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
 /* KGSL Simple GPU Governor */
 /* Copyright (c) 2011-2013, Paul Reioux (Faux123). All rights reserved. */
-static int laziness = 5;
-module_param_named(simple_laziness, laziness, int, 0664);
-
+static int lazyness = 5;
 static int ramp_up_threshold = 6000;
-module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
 static int simple_governor(struct kgsl_device *device, int idle_stat)
 {
@@ -173,13 +166,13 @@ static int simple_governor(struct kgsl_device *device, int idle_stat)
 	} else {
 		if ((pwr->active_pwrlevel >= 0) &&
 			(pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
-			if (laziness > 0) {
+			if (lazyness > 0) {
 				/* hold off for a while */
-				laziness--;
+				lazyness--;
 				val = 0; /* don't change anything yet */
 			} else {
 				val = 1; /* above min, lower it */
-				laziness = 5; /* reset laziness count */
+				lazyness = 5; /* reset lazyness count */
 			}
 		else if (pwr->active_pwrlevel == (pwr->num_pwrlevels - 1))
 			val = 0; /* already @ min, so do nothing */

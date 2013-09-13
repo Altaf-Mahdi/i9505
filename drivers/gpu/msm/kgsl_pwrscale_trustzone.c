87b7f7147f98c5e7019c14e20583b3f97bf55f21
Francisco Franco <franciscofranco.1990@gmail.com>
2013-09-13 02:25:35 +0100

    kgsl: add interactive algorithm merged from my changes on mako FK.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_pwrscale_trustzone.c

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -8,7 +8,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 #include <linux/export.h>
@@ -18,6 +17,8 @@
 #include <linux/spinlock.h>
 #include <mach/socinfo.h>
 #include <mach/scm.h>
+#include <linux/module.h>
+#include <linux/jiffies.h>
 
 #include "kgsl.h"
 #include "kgsl_pwrscale.h"
@@ -25,6 +26,9 @@
 
 #define TZ_GOVERNOR_PERFORMANCE 0
 #define TZ_GOVERNOR_ONDEMAND    1
+#define TZ_GOVERNOR_INTERACTIVE	2
+
+#define DEBUG 0
 
 struct tz_priv {
 	int governor;
@@ -48,6 +52,7 @@ spinlock_t tz_lock;
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
 
+#if 0
 #ifdef CONFIG_MSM_SCM
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry(u32 cmd, u32 val, u32 id)
@@ -65,6 +70,16 @@ static int __secure_tz_entry(u32 cmd, u32 val, u32 id)
 	return 0;
 }
 #endif /* CONFIG_MSM_SCM */
+#endif
+
+unsigned long window_time = 0;
+unsigned long sample_time_ms = 100;
+unsigned int up_threshold = 50;
+unsigned int down_threshold = 25;
+
+module_param(sample_time_ms, long, 0664);
+module_param(up_threshold, int, 0664);
+module_param(down_threshold, int, 0664);
 
 static ssize_t tz_governor_show(struct kgsl_device *device,
 				struct kgsl_pwrscale *pwrscale,
@@ -75,6 +90,8 @@ static ssize_t tz_governor_show(struct kgsl_device *device,
 
 	if (priv->governor == TZ_GOVERNOR_ONDEMAND)
 		ret = snprintf(buf, 10, "ondemand\n");
+    	else if (priv->governor == TZ_GOVERNOR_INTERACTIVE)
+		ret = snprintf(buf, 11, "interactive\n");
 	else
 		ret = snprintf(buf, 13, "performance\n");
 
@@ -98,6 +115,8 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 
 	if (!strncmp(str, "ondemand", 8))
 		priv->governor = TZ_GOVERNOR_ONDEMAND;
+    	else if (!strncmp(str, "interactive", 11))
+		priv->governor = TZ_GOVERNOR_INTERACTIVE;
 	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
@@ -121,11 +140,7 @@ static struct attribute_group tz_attr_group = {
 
 static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
-	struct tz_priv *priv = pwrscale->priv;
-	if (device->state != KGSL_STATE_NAP &&
-		priv->governor == TZ_GOVERNOR_ONDEMAND)
-		kgsl_pwrctrl_pwrlevel_change(device,
-					device->pwrctrl.default_pwrlevel);
+	return;
 }
 
 static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
@@ -133,7 +148,8 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	struct tz_priv *priv = pwrscale->priv;
 	struct kgsl_power_stats stats;
-	int val, idle;
+	unsigned long total_time_ms = 0;
+	unsigned long busy_time_ms = 0;
 
 	/* In "performance" mode the clock speed always stays
 	   the same */
@@ -143,28 +159,37 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	device->ftbl->power_stats(device, &stats);
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
-	/* Do not waste CPU cycles running this algorithm if
-	 * the GPU just started, or if less than FLOOR time
-	 * has passed since the last run.
-	 */
-	if ((stats.total_time == 0) ||
-		(priv->bin.total_time < FLOOR))
+
+	if (time_is_after_jiffies(window_time + msecs_to_jiffies(sample_time_ms)))
 		return;
 
-	/* If the GPU has stayed in turbo mode for a while, *
-	 * stop writing out values. */
-	if (pwr->active_pwrlevel == 0) {
-		if (priv->no_switch_cnt > SWITCH_OFF) {
-			priv->skip_cnt++;
-			if (priv->skip_cnt > SKIP_COUNTER) {
-				priv->no_switch_cnt -= SWITCH_OFF_RESET_TH;
-				priv->skip_cnt = 0;
-			}
-			return;
-		}
-		priv->no_switch_cnt++;
-	} else {
-		priv->no_switch_cnt = 0;
+	total_time_ms = jiffies_to_msecs((long)jiffies - (long)window_time);
+
+	/*
+	 * We're casting u32 here because busy_time is s64 and this would be a
+	 * 64-bit division and we can't do that on a 32-bit arch 
+	 */
+	busy_time_ms = (u32)priv->bin.busy_time / USEC_PER_MSEC;
+
+#if DEBUG
+	pr_info("GPU current load: %ld\n", busy_time_ms);
+	pr_info("GPU total time load: %ld\n", total_time_ms);
+	pr_info("GPU frequency: %d\n", pwr->pwrlevels[pwr->active_pwrlevel].gpu_freq);
+#endif
+
+	if ((busy_time_ms * 100) > (total_time_ms * up_threshold))
+	{
+		if ((pwr->active_pwrlevel > 0) &&
+			(pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
+			kgsl_pwrctrl_pwrlevel_change(device,
+					     pwr->active_pwrlevel - 1);
+	}
+	else if ((busy_time_ms * 100) < (total_time_ms * down_threshold))
+	{
+		if ((pwr->active_pwrlevel >= 0) &&
+			(pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
+			kgsl_pwrctrl_pwrlevel_change(device,
+					     pwr->active_pwrlevel + 1);
 	}
 
 	/* If there is an extended block of busy processing,
@@ -179,6 +204,7 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	}
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
+	window_time = jiffies;
 	if (val)
 		kgsl_pwrctrl_pwrlevel_change(device,
 					     pwr->active_pwrlevel + val);
@@ -193,12 +219,7 @@ static void tz_busy(struct kgsl_device *device,
 static void tz_sleep(struct kgsl_device *device,
 	struct kgsl_pwrscale *pwrscale)
 {
-	struct tz_priv *priv = pwrscale->priv;
-
-	__secure_tz_entry(TZ_RESET_ID, 0, device->id);
-	priv->no_switch_cnt = 0;
-	priv->bin.total_time = 0;
-	priv->bin.busy_time = 0;
+	return;
 }
 
 #ifdef CONFIG_MSM_SCM
@@ -210,7 +231,7 @@ static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	if (pwrscale->priv == NULL)
 		return -ENOMEM;
 
-	priv->governor = TZ_GOVERNOR_ONDEMAND;
+	priv->governor = TZ_GOVERNOR_INTERACTIVE;
 	spin_lock_init(&tz_lock);
 	kgsl_pwrscale_policy_add_files(device, pwrscale, &tz_attr_group);
 

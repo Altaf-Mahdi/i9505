e8ee1d51710931e6f355fa28ab24319a2d71da9b
ktoonsez <ktoonsez@gmail.com>
2013-12-09 20:08:07 -0700

    Linux 3.4.73
    Conflicts:
    
    	block/elevator.c
    	net/ipv4/ping.c
    
    Conflicts:
    
    	net/ipv4/ping.c

--- block/elevator.c
+++ block/elevator.c
@@ -1036,7 +1036,7 @@ fail_register:
 /*
  * Switch this queue to the given IO scheduler.
  */
-int elevator_change(struct request_queue *q, const char *name)
+static int __elevator_change(struct request_queue *q, const char *name)
 {
 	char elevator_name[ELV_NAME_MAX];
 	struct elevator_type *e;
@@ -1057,6 +1057,18 @@ int elevator_change(struct request_queue *q, const char *name)
 	}
 
 	return elevator_switch(q, e);
+}
+
+int elevator_change(struct request_queue *q, const char *name)
+{
+	int ret;
+
+	/* Protect q->elevator from elevator_init() */
+	mutex_lock(&q->sysfs_lock);
+	ret = __elevator_change(q, name);
+	mutex_unlock(&q->sysfs_lock);
+
+	return ret;
 }
 EXPORT_SYMBOL(elevator_change);
 
